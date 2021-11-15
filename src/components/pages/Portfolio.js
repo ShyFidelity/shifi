cf0121d9f232d48e45949cf7db166e36164d81ee
jkworks <info@jilliankayworks.com>
2021-11-15 11:51:57 -0700

    css

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -7,19 +7,20 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import '../../styles/portCard.css'
 import Snake from '../images/snakeBG.svg'
+
 export default function Portfolio() {
   return (
     <div className = "cardContainer">
     <Card className="card" sx={{ maxWidth: 345 , maxHeight: 345}}>
     <CardMedia
         component="img"
-        height="140"
+        height="200"
         image= { Snake }
         alt="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Ourobos 
+          Ouroboros
         </Typography>
         <Typography variant="body2" color="text.primary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
