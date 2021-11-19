6497ad5c2231a6f697123ab4f33d1fe996ca1563
jkworks <info@jilliankayworks.com>
2021-11-19 14:29:09 -0700

    contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,97 @@
-import React from "react";
+
 import contact from "../images/phoneafriendsmall.png";
 import '../../styles/About.css'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from 'react';
+import '../../styles/Contact.css'
 
+
+const subjects = [
+  {
+    value: 'Work',
+    label: 'Work',
+  },
+  {
+    value: 'Collaborate',
+    label: 'Collaborate',
+  },
+  {
+    value: '<3',
+    label: '<3',
+  },
+  {
+    value: 'question',
+    label: 'question',
+  },
+
+];
 export default function Contact() {
+
+  const [subject, setSubject] = React.useState('EUR');
+
+  const handleChange = (event) => {
+    setSubject(event.target.value);
+  };
+
+
   return (
     <div className="page">
       <h1>Contact Page</h1>
-      <img src={contact} />
-      <ul>
-        <li>
+      <img src={contact}  width="150" height= "300" alt="hand holding phone"/>
+      <Box
+        className="contactForm"  
+        component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+  
+          <div>
+            <TextField
+              required
+              id="filled-required"
+              label="Required"
+              defaultValue="Name"
+              variant="filled"
+            />
+
+<TextField
+              required
+              id="filled-required"
+              label="Required"
+              defaultValue="email"
+              variant="filled"
+            />
+            <TextField
+          id="filled-select-currency"
+          select
+          label="Select"
+          value={subjects}
+          onChange={handleChange}
+          helperText="Subject"
+          variant="filled"
+        >
+          {subjects.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          id="filled-multiline-static"
+          label="Tell me..."
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+          variant="filled"
+        />
+       </div>
+           
+        </Box>
           {" "}
           <a
             href="tel:3039013230"
@@ -16,13 +99,8 @@ export default function Contact() {
           >
             <p class="call-button">303-901-3230</p>
           </a>
-        </li>
-        <li>
-          <a href="mailto:shyfidelity100@gmail.com">shyfidelity100@gmail.com</a>
-        </li>
        
-      </ul>
-  
+          <a href="mailto:shyfidelity100@gmail.com">shyfidelity100@gmail.com</a>
       <div class="socialimages">
        <a href= "https://www.linkedin.com/in/jillianfitzmaurice100/" target="_blank">link</a> 
        <a href = "https://www.instagram.com/jilliankayworks/" target="_blank">insta</a> 
